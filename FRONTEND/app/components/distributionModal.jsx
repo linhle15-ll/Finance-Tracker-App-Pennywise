eb9e24fefeaaac29249e8561b5bae9f864a2020d
Ngoc Linh Le <144966197+linhle15-ll@users.noreply.github.com>
2024-08-26 01:21:59 +0700

    Update distributionModal.jsx

--- FRONTEND/app/components/distributionModal.jsx
+++ FRONTEND/app/components/distributionModal.jsx
@@ -23,12 +23,12 @@ const DistributionModal = ({ id, open, setOpen }) => {
                 cancelText="Cancel"
 
             >
-                <form className = "flex flex-col gap-3.5 max-h-[450px] overflow-y-scroll">
+                <form className="flex flex-col gap-3.5 max-h-[450px] overflow-y-scroll">
                     <div> 
-                        <div> Your balance is now <span style = {{fontWeight: "600"}}> $ {getBalance()}. </span> </div>
+                        <div> Your balance is now <span className="font-600"> $ {getBalance()}. </span> </div>
                         <span className = "text-darkGreen font-600 font-italic"> How much % of balance is to be distributed to your budget? </span> 
                     </div> 
-                    <input className = "border border-gray-400 p-2 rounded-[10px] mr-2 focus:outline-none focus:border-mintGreen" required
+                    <input className="border border-gray-400 p-2 rounded-[10px] mr-2 focus:outline-none focus:border-mintGreen" required
                         type = "number" 
                         onChange={(e) => handleDistributionChange("distribution", e.target.value)} 
                         placeholder = "Distribution Percentage" 
